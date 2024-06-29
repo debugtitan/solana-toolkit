0a5695651ade7b1dc1ca136e8401afdc394f74f7
debugtitan <alts.devs@gmail.com>
2024-06-29 10:08:24 +0100

    wallet initialization

---
+++ soloana-toolkit/wallet.py
@@ -0,0 +1,16 @@
+from typing import Any, Dict, Union
+
+
+class Wallet:
+    """"""
+    
+    def generate(self, password):
+        """"""
+        
+    @staticmethod
+    def encrypt(private_key, password):
+        """"""
+        
+    @staticmethod
+    def decrypt(keyfile: Union[str, Dict[str, Any]], password: str):
+        """"""
